41f988239dde6e7a8301b5c079143606dc60019e
Cris <cristovaom22@gmail.com>
2023-05-27 19:04:40 -0300

    Criacao da classe veiculoDTO

--- src/main/java/br/com/uniamerica/estacionamento/dto/VeiculoDTO.java
+++ src/main/java/br/com/uniamerica/estacionamento/dto/VeiculoDTO.java
@@ -1,6 +1,11 @@
 package br.com.uniamerica.estacionamento.dto;
 
+import br.com.uniamerica.estacionamento.entity.Cor;
+import br.com.uniamerica.estacionamento.entity.Modelo;
+import br.com.uniamerica.estacionamento.entity.Tipo;
+import br.com.uniamerica.estacionamento.entity.Veiculo;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
@@ -11,4 +16,33 @@ public class VeiculoDTO extends AbstractEntityDTO{
     @Size(min = 6,max = 10,message = "Erro tamanho no maximo de 6 a 10 caracteres!")
     @Pattern(regexp = "^[A-Z]{3}[0-9][0-9A-Z][0-9]{2}$", message = "Placa invalida! favor digitar uma placa valida!")
     private String placa;
+
+    @Getter
+    @NotNull(message = "Modelo não pode ser nulo!")
+    private Modelo modelo;
+    @Getter
+    @NotNull(message = "Cor não pode ser invalida!")
+    private Cor cor;
+    @Getter
+    @NotNull(message = "Ano invalido!")
+    private int ano;
+
+    @Getter
+    @NotNull(message = "Tipo invalido!")
+    private Tipo tipo;
+
+    public VeiculoDTO(String placa, Modelo modelo, Cor cor, int ano, Tipo tipo) {
+        this.placa = placa;
+        this.modelo = modelo;
+        this.cor = cor;
+        this.ano = ano;
+        this.tipo = tipo;
+    }
+
+    public VeiculoDTO(Veiculo veiculo){
+        placa = veiculo.getPlaca();
+        modelo = veiculo.getModelo();
+        cor = veiculo.getCor();
+        ano = veiculo.getAno();
+    }
 }
